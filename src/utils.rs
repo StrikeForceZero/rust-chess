62a1a69baac88b9a9f8b330358d7c6a5348c9b8c
bstriker <strikeforcezero@gmail.com>
2023-11-08 23:25:44 -0500

    draft: Initial implementation

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,25 @@ pub struct CustomStructIterator<'a, T> {
     pub index: usize,
 }
 
+impl<T> CustomStructIterator<'_, T> {
+    pub const fn from(data: &T) -> Self<T> {
+        Self {
+            data,
+            index: 0,
+        }
+    }
+}
+
 pub struct CustomStructIteratorMut<'a, T> {
     pub data: &'a mut T,
     pub index: usize,
+}
+
+impl<T> CustomStructIteratorMut<'_, T> {
+    pub const fn from(data: &mut T) -> Self<T> {
+        Self {
+            data,
+            index: 0,
+        }
+    }
 }
